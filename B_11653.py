c2c2b2d91f0f2741e36b1b884f4d62cffad64898
eunjng5474 <eunjng5474@gmail.com>
2023-01-29 23:45:16 +0900

    230129 2345

--- B_11653.py
+++ B_11653.py
@@ -1,14 +1,19 @@
 n = int(input())
+if n == 1:
+    print('')
 
 # n_lst = []
 
 def func(num):
+    if num == 1:
+        pass
     for i in range(2, num+1):
         if num % i == 0 and num//i == 1:
             return i
         elif num % i == 0:
             print(i)
             return func(num//i)
+            # break
             
     # elif n == 1:
     # print('')
