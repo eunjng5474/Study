7bdb45b0b1fb2bd1b26462f66c0e3c8a7b85f434
eunjng5474 <eunjng5474@gmail.com>
2023-01-30 10:34:51 +0900

    230130 1034

--- B_11653.py
+++ B_11653.py
@@ -1,27 +1,43 @@
-n = int(input())
-if n == 1:
-    print('')
+# n = int(input())
+# if n == 1:
+#     print('')
 
-# n_lst = []
+# # n_lst = []
 
-def func(num):
-    if num == 1:
-        pass
-    for i in range(2, num+1):
-        if num % i == 0 and num//i == 1:
-            return i
-        elif num % i == 0:
-            print(i)
-            return func(num//i)
-            # break
+# def func(num):
+#     if num == 1:
+#         pass
+#     for i in range(2, num+1):
+#         if num % i == 0 and num//i == 1:
+#             return i
+#         elif num % i == 0:
+#             print(i)
+#             return func(num//i)
+#             # break
             
-    # elif n == 1:
-    # print('')
+#     # elif n == 1:
+#     # print('')
 
-print(func(n))
+# print(func(n))
 
 
 
 
 ## sol 2
-# while n 
+n = int(input())
+
+while n != 0:
+    i = 2
+    if n == 1:
+        break
+    # for i in range(2, n+1):
+    while i <= n:
+        if n % i == 0:
+            print(i)
+            n = n // i
+        else:
+            i += 1
+    # for i in range(2, n+1):
+    #     if n % i == 0:
+    #         print(i)
+    #         n = n // i
