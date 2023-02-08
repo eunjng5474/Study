81bca2d131b9c033c60c490d0865b0461836a184
eunjng5474 <eunjng5474@gmail.com>
2023-02-08 17:14:03 +0900

    230208 1714

--- week2/B_2869.py
+++ week2/B_2869.py
@@ -2,10 +2,18 @@ A, B, V = map(int, input().split())
 
 now = 0
 cnt = 0
-while True:
-    now += A
-    now -= B
-    if now >= V:
-        break
-    cnt += 1
-print(cnt)
+# while True:
+#     if now >= V:
+#         break
+#     now += A
+#     now -= B
+#     cnt += 1
+# print(cnt)
+
+
+# while True:
+#     cnt = V // (A-B)
+#     if (A-B) * cnt + A >= V:
+#         cnt += 1
+#         break
+# print(cnt)
