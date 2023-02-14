feaad6d9c949aa702f5ecd5ef1ebb5f3a35706ad
eunjng5474 <eunjng5474@gmail.com>
2023-02-14 17:17:40 +0900

    230214 1717

---
+++ week3/B_11053.py
@@ -0,0 +1,62 @@
+N = int(input())
+ai = list(map(int, input().split()))
+stack = []
+stack_lst = []
+
+# stack.append(ai[0])
+# for i in range(len(ai)):
+
+i = 0
+while i < N:
+    if i == 0:
+        stack.append(ai[i])
+    elif ai[i] > stack[-1]:
+        stack.append(ai[i])
+    stack_lst.append(stack)
+    i += 1
+
+result = 0
+for s in stack_lst:
+    if len(s) > result:
+        result = len(s)
+print(len(s))    ### 최대값 출력하기
+
+
+# i = 1
+# for j in range(i+1, len(ai)):
+#     if ai[j] > stack[-1]:
+#         stack.append(ai[j])
+#     i += 1
+# stack_lst.append(stack)
+
+    # if i == 0:
+    #     stack.append(ai[i])
+
+    # if ai[i] > stack[-1]:
+    #     stack.append(ai[i])
+    # else:
+    #     stack_lst.append(stack)
+    #     stack = []
+    #     stack.append(ai[i])
+
+# print(stack)
+# print(stack_lst)
+
+# print(len(stack))    ### 최대값
+
+##### 0부터 끝까지 스택에 넣고 그걸 스택리스트에 넣고
+## 그다음 1부터 끝까지, 2부터 , ...
+
+
+# ### '가장 긴' 빼먹은 코드
+# N = int(input())
+# ai = list(map(int, input().split()))
+# stack = []
+
+# stack.append(ai[0])
+# for i in range(1, len(ai)):
+#     if ai[i] > stack[-1]:
+#         stack.append(ai[i])
+
+# print(len(stack))
+
