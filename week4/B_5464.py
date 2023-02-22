60bc1c5fc7046bafb000589f463c1ede65adf08c
eunjng5474 <eunjng5474@gmail.com>
2023-02-22 17:51:04 +0900

    230222 1751

--- week4/B_5464.py
+++ week4/B_5464.py
@@ -4,37 +4,50 @@ from collections import deque
 N, M = map(int, input().split())
 # parking = [x for x in range(1, N+1)]
 # cars = x for 
-parking = {}
-cars = {}
-park_lst = deque()
+parking_fee = {}
+car_weight = {}
+parking = [[] for _ in range(N)]
 q = deque()
 result = 0
 
 for n in range(N):
-    parking[n+1] = int(input())
+    parking_fee[n] = int(input())
 for m in range(M):
-    cars[m+1] = int(input())
+    car_weight[m] = int(input())
 
-print(parking)
-print(cars)
+print(parking_fee)
+print(car_weight)
 
 for i in range(2*M):
     x = int(input())
 
     if x > 0:
-        if len(park_lst) == N:
+        if parking.count(0) == 0:           #####이 조건 수정!!!!!
             q.append(x)
-        else: 
-            ## 리스트에서 빈 자리에 왼쪽부터 넣도록 수정
-            park_lst.append(x)
-            result += (parking[x] * cars[x])
+        else:
+            for p in range(N):
+                if p == 0:
+                    parking[p].append(x)
+                    # p_idx = parking.index(x)
+                    result += (parking_fee[p] * car_weight[p])
     else:
         x = abs(x)
-        x_idx = park_lst.index(x)
-        park_lst.pop(x_idx)
+        for p in parking:
+            if p == x:
+                x_idx = p
+        # x_idx = parking.index(x)
+        parking.remove(x)
+
+        if q:
+            parking.insert(parking[p], q.popleft())
+            result += (parking_fee[p] * car_weight[p])
+    
+    print('parking: ', parking)
+    print('q: ', q)
+
         # incar = q.popleft()
-        # park_lst.append(incar)
-        # result += (parking[incar] * cars[incar])
+        # parking.append(incar)
+        # result += (parking_fee[incar] * car_weight[incar])
 
 print(result)
 
