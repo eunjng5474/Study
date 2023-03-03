74128e632bff7754cf05117c9d66be38d5473e65
eunjng5474 <eunjng5474@gmail.com>
2023-03-03 22:39:31 +0900

    230303 2239

--- week5/B_10158_s4.py
+++ week5/B_10158_s4.py
@@ -6,5 +6,17 @@ p, q = map(int, input().split())
 t = int(input())
 
 tp = (p + t) // w
-tq = (q + t) // w
+tq = (q + t) // h
+
+if not tp % 2:
+    x = (p+t) % w
+else:
+    x = w - ((p+t) % w)
+
+if not tq % 2:
+    y = (q+t) % h
+else:
+    y = h - ((q+t) % h)
+
+print(x, y)
 
--- week5/B_2564_s1.py
+++ week5/B_2564_s1.py
@@ -8,26 +8,46 @@ x, y = map(int, input().split())
 result = 0
 
 for i in range(N):
-    if lst[i][0] == x:
+    if lst[i][0] == x:                  # 같은 방향일 때
         result += abs(lst[i][1] - y)
-    elif [lst[i][0], x] in [[1, 2], [2, 1]]:
+    elif [lst[i][0], x] in [[1, 2], [2, 1]]:    # (남-북)일 때
         d1 = y + lst[i][1]
         d2 = abs(w-y) + abs(w-lst[i][1])
-        print('d1: ', d1, 'd2: ', d2)
         result += min(d1, d2)
         result += h
-    elif [lst[i][0], x] in [[3, 4], [4, 3]]:
+    elif [lst[i][0], x] in [[3, 4], [4, 3]]:    #(동-서)일 때
         d1 = y + lst[i][1]
         d2 = abs(h-y) + abs(h-lst[i][1])
-        print('d1: ', d1, 'd2: ', d2)
         result += min(d1, d2)
         result += w
 
-    else:
-        if x in [1, 2]:
-            d1 = y + lst[i][1]
-            d2 =
-# print(result)
+    else:           # (북-서)와 같이 코너를 중심으로 꺾여있을 때
+        if x == 1:
+            result += lst[i][1]
+            if lst[i][0] == 3:
+                result += y
+            elif lst[i][0] == 4:
+                result += (w-y)
+        elif x == 2:
+            result += (h - lst[i][1])
+            if lst[i][0] == 3:
+                result += y
+            elif lst[i][0] == 4:
+                result += (w-y)
+        elif x == 3:
+            result += lst[i][1]
+            if lst[i][0] == 1:
+                result += y
+            elif lst[i][0] == 2:
+                result += (h-y)
+        elif x == 4:
+            result += (w - lst[i][1])
+            if lst[i][0] == 1:
+                result += y
+            elif lst[i][0] == 2:
+                result += (h-y)
+
+print(result)
 
 
 
