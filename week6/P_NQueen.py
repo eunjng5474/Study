d147b6f96db2c30f7238fbcddfe0b84a19506843
eunjng5474 <eunjng5474@gmail.com>
2023-03-12 20:24:45 +0900

    230312

--- week6/P_NQueen.py
+++ week6/P_NQueen.py
@@ -1,33 +1,89 @@
-N = 4
-board = [0] * N
-visited = [0] * N
-result = 0
+answer = 0
 
-def check(n):
-    for i in range(n):    # n까지 줄 i에 대해 모두 확인
-        # n행의 퀸이 있는 열과 i행의 퀸이 있는 열이 같거나
-        # 행의 차이만큼 퀸이 있는 열의 차이가 같다면 대각선에 위치하는 것이므로 False
-        if board[n] == board[i] or abs(board[n] - board[i]) == (n-i):
+
+def check(x, y, queen):
+    for i in range(x):
+        if y == queen[i] or abs(y - queen[i]) == (x - i):
             return False
     return True
 
-def nqueen(n):
-    global result
-    if n == N:          # 행이 N이 되면 N개의 퀸 모두 놓음
-        result += 1
+    # for i in range(n):    # n까지 줄 i에 대해 모두 확인
+    #     # n행의 퀸이 있는 열과 i행의 퀸이 있는 열이 같거나
+    #     # 행의 차이만큼 퀸이 있는 열의 차이가 같다면 대각선에 위치하는 것이므로 False
+    #     if board[n] == board[i] or abs(board[n] - board[i]) == (n-i):
+    #         return False
+    # return True
+
+
+def nqueen(x, n, queen):
+    global answer
+    if x == n:
+        answer += 1
         return
 
-    for i in range(N):
-        if not visited[i]:
-            board[n] = i    # n행의 i열에 퀸을 놓았다는 표시
+    for y in range(n):
+        if check(x, y, queen):
+            queen[x] = y
+            nqueen(x + 1, n, queen)
 
-            if check(n):
-                visited[i] = 1  # 방문 표시 후 다음 행 조사
-                nqueen(n+1)
-                visited[i] = 0  # 다시 0으로 설정
 
-nqueen(0)
-print(result)
+#     if n == N:          # 행이 N이 되면 N개의 퀸 모두 놓음
+#         answer += 1
+#         return
+
+#     for i in range(N):
+#         if not visited[i]:
+#             board[n] = i    # n행의 i열에 퀸을 놓았다는 표시
+
+#             if check(n):
+#                 visited[i] = 1  # 방문 표시 후 다음 행 조사
+#                 nqueen(n+1)
+#                 visited[i] = 0  # 다시 0으로 설정
+
+def solution(n):
+    global answer
+    queen = [0] * n
+    # visited = [0] * n
+
+    nqueen(0, n, queen)
+
+    return answer
+
+
+
+
+
+###########
+# N = 4
+# board = [0] * N
+# visited = [0] * N
+# result = 0
+#
+# def check(n):
+#     for i in range(n):    # n까지 줄 i에 대해 모두 확인
+#         # n행의 퀸이 있는 열과 i행의 퀸이 있는 열이 같거나
+#         # 행의 차이만큼 퀸이 있는 열의 차이가 같다면 대각선에 위치하는 것이므로 False
+#         if board[n] == board[i] or abs(board[n] - board[i]) == (n-i):
+#             return False
+#     return True
+#
+# def nqueen(n):
+#     global result
+#     if n == N:          # 행이 N이 되면 N개의 퀸 모두 놓음
+#         result += 1
+#         return
+#
+#     for i in range(N):
+#         if not visited[i]:
+#             board[n] = i    # n행의 i열에 퀸을 놓았다는 표시
+#
+#             if check(n):
+#                 visited[i] = 1  # 방문 표시 후 다음 행 조사
+#                 nqueen(n+1)
+#                 visited[i] = 0  # 다시 0으로 설정
+#
+# nqueen(0)
+# print(result)
 
 
 
