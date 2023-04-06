848046dc9345be3dcb8298613bdf32e179284f55
HB Kim <ghkdwp129@gmail.com>
2023-04-06 17:56:35 +0900

    230406 1756

---
+++ week9/B_11718.java
@@ -0,0 +1,17 @@
+package algo;
+
+import java.util.Scanner;
+public class B_11718 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			String str = sc.next();
+			System.out.println(str);
+			
+		}
+		
+	}
+
+}
---
+++ week9/B_2743.java
@@ -0,0 +1,16 @@
+package algo;
+
+import java.util.Scanner;
+public class B_2743 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		int len_str = str.length();
+		
+		System.out.println(len_str);
+
+	}
+
+}
